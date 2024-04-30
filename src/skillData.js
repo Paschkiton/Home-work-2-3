a55e29b7304b8e537041d1fd6e1a1a4a27e454c9
Paschkiton <paschkiton@mail.ru>
2024-04-30 11:29:17 +0500

    Добавил map

---
+++ src/skillData.js
@@ -0,0 +1,11 @@
+export const skillsData = [
+  { name: "JavaScript", description: "Язык программирования", level: 90 },
+  {
+    name: "React",
+    description: "Библиотека JavaScript для создания интерфейсов",
+    level: 80,
+  },
+  { name: "HTML", description: "Язык разметки веб-страниц", level: 75 },
+  { name: "CSS", description: "Язык стилей для веб-страниц", level: 65 },
+  { name: "Git", description: "Система контроля версий", level: 50 },
+];
